88ac9fac540e693d928efc28152611310dc8eb99
Durbaa <143736812+DDurbaa@users.noreply.github.com>
2024-01-28 20:26:43 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
     var winScroll = $(this).scrollTop();
-    
-    $('.bg').css({
-        'transform': 'translate(0, '+ winScroll / 8 + '%)'
+
+    $('.bg1').css({
+        'transform': 'translate(0, ' + winScroll / 0 + '%)'
     });
     $('.bg2').css({
-        'transform': 'translate(0, '+ winScroll / 12 + '%)'
+        'transform': 'translate(0, ' + winScroll / 22 + '%)'
     });
     $('.bg3').css({
-        'transform': 'translate(0, '+ winScroll / 18 + '%)'
+        'transform': 'translate(0, ' + winScroll / 16 + '%)'
     });
     $('.bg4').css({
-        'transform': 'translate(0, '+ winScroll / 26 + '%)'
+        'transform': 'translate(0, ' + winScroll / 14 + '%)'
     });
 });
