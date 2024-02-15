8174523eda43b02a3034b163ed5d89ca1b41b584
simyxx <simonecek2005@gmail.com>
2024-02-15 18:22:39 +0100

    Pause menu

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] Rigidbody2D player;
+    private bool pause;
+    void Start()
+    {
+        pause = false;
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
@@ -13,5 +22,31 @@ public class GameManager : MonoBehaviour
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pause)
+            {
+                pause = true;
+                PauseGame();
+            }
+            else 
+            {
+                pause = false;
+                ResumeGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
     }
 }
