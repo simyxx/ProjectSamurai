69db13b9c305877d7c218544575119b2392c52ab
Durbaa <143736812+DDurbaa@users.noreply.github.com>
2024-04-02 17:08:21 +0200

    Add files via upload

---
+++ rengekii/script.js
@@ -0,0 +1,17 @@
+$(window).scroll(function () {
+    var winScroll = $(this).scrollTop();
+
+    $('.bg1').css({
+        'transform': 'translate(0, ' + winScroll / 0 + '%)'
+    });
+    $('.bg2').css({
+        'transform': 'translate(0, ' + winScroll / 22 + '%)'
+    });
+    $('.bg3').css({
+        'transform': 'translate(0, ' + winScroll / 16 + '%)'
+    });
+    $('.bg4').css({
+        'transform': 'translate(0, ' + winScroll / 14 + '%)'
+    });
+});
+
