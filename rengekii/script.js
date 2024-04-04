7da6295bcf8c5b8f4c864299f0ebde32681594e5
Durbaa <143736812+DDurbaa@users.noreply.github.com>
2024-04-04 18:47:48 +0200

    auaoaohao
    he

--- rengekii/script.js
+++ rengekii/script.js
@@ -14,4 +14,13 @@ $(window).scroll(function () {
         'transform': 'translate(0, ' + winScroll / 14 + '%)'
     });
 });
-
+function downloadFile() {
+    const filePath = 'B07_betatest.zip';
+    
+    const anchorElement = document.createElement('a');
+    anchorElement.href = filePath;
+    anchorElement.download = ''; 
+    document.body.appendChild(anchorElement); 
+    anchorElement.click(); 
+    document.body.removeChild(anchorElement); 
+}
